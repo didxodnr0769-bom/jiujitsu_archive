1df4732a51bb36b9b1700b0b02c5dea25ad90032
taewook <didxodnr0769@gmail.com>
2025-12-27 22:44:14 +0900

    feat : tanstack query 설정 및 설치

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,13 @@
 import AppRoute from '../shared/route/AppRoute';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
-  return <AppRoute />;
+  return (
+    <>
+      <AppRoute />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.tsx";
 import "./styles/index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./shared/query/queryClient.js";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
